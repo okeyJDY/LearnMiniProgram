2308858a6c4c2b265617d2ad4e1b0803ed236544
OkeyJDY <okeyjdy@163.com>
2019-11-17 01:03:53 +0800

    小程序初体验

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,66 +1,27 @@
-// miniprogram/pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'jdy',
+    age: 18,
+    students: [
+      { id: '001', name: 'kobe', age: 30 },
+      { id: '002', name: 'james', age: 28 },
+      { id: '003', name: 'curry', age: 20 },
+      { id: '004', name: 'jdy', age: 18 },
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+	handleBtnClick() {
+		let {counter} = this.data
+		counter++
+		this.setData({
+			counter
+		})
+	},
+	handleSubTraction() {
+		let {counter} = this.data
+		counter--
+		this.setData({
+			counter
+		})
+	}
 })
