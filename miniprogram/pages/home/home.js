2075f9ae72e0463b96302ef7f804fff3815e3d49
OkeyJDY <okeyjdy@163.com>
2019-11-22 13:16:20 +0800

    网络请求封装

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,66 +1,62 @@
 // miniprogram/pages/home/home.js
-Page({
+import request from '../../service/network'
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
 
   },
+  onLoad: function(options) {
+    this.get_data_origin()
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    request({
+      url: 'http://httpbin.org/post',
+      method: 'post',
+      data: {
+        name: 'coderwhy',
+        age: 18
+      }
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  get_data_origin() {
+    // 1.简单的get请求
+    // wx.request({
+    //   url: 'http://106.54.54.237:8000/api/v1/recommend',
+    //   success: (res) => {
+    //     console.log(res);
+    //   }
+    // })
 
-  },
+    // 2.发送get请求, 并携带参数
+    // wx.request({
+    //   url: 'http://106.54.54.237:8000/api/v1/home/data',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success: (res) => {
+    //     console.log(res);
+    //   }
+    // })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 3.发送post请求, 并携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: 'post',
+    //   data: {
+    //     name: 'coderwhy',
+    //     age: 18
+    //   },
+    //   success: (res) => {
+    //     console.log(res);
+    //   },
+    //   fail: (err) => {
+    //     console.log(err);
+    //   }
+    // })
   }
 })
